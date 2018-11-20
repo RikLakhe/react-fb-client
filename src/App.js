ddd97862322278e982005683951f30da1677671c
riklakhe <rikeshlakhe@gmail.com>
2018-11-20 12:43:25 +0545

    first part

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import AppNavBar from './components/layout/AppNavBar'
+import Dashboard from './components/layout/Dashboard'
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <AppNavBar />
+          <div className="container">
+            <Switch>
+            <Route exact path="/" component={Dashboard} />
+            </Switch>
+          </div>
+          
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Clients from '../clients/Clients';
+import SideBar from '../layout/SideBar';
+
+
+export default function () {
+  return (
+    <div>
+      <div className="row">
+      <div className="col-md-10">
+      <Clients />
+      </div>
+      <div className="col-md-2">
+      <SideBar />
+      </div>
+      </div>
+    </div>
+  )
+}
